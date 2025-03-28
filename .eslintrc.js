c021ec51951a96c1cda38e7b9a6e2a2e74a2393a
Ha DANG <dvietha@gmail.com>
2025-03-28 19:10:05 +0700

    Disable no-explicit-any ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { 
       'argsIgnorePattern': '^_',
       'varsIgnorePattern': '^_'
